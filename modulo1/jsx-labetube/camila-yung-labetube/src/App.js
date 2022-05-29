62b82708a8323ae3ff40371edcd190ea9fcb40e0
Camila Yung Alves <camilayung92@gmail.com>
2022-05-29 19:28:20 -0300

    Versão final

--- modulo1/jsx-labetube/camila-yung-labetube/src/App.js
+++ modulo1/jsx-labetube/camila-yung-labetube/src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  const reproduzVideo = () => {
+    alert("O vídeo está sendo reproduzido")
+}
+
   return (
     <div className="App">
       <header className="App-header">
@@ -23,40 +28,44 @@ function App() {
         </nav>
 
         <section className="App-painel-de-videos">
-                <div className="App-box-pagina-principal media1" onClick="reproduzVideo()">
-                    <img src="https://picsum.photos/400/400?a=1 " alt="" />
-                    <h4>Título do vídeo</h4>
-                </div>
-                <div className="App-box-pagina-principal media2" onClick="reproduzVideo()">
-                    <img src="https://picsum.photos/400/400?a=2 " alt="" />
-                    <h4>Título do vídeo</h4>
-                </div>
-                <div className="App-box-pagina-principal media3" onClick="reproduzVideo()">
-                    <img src="https://picsum.photos/400/400?a=3 " alt="" />
-                    <h4>Título do vídeo</h4>
-                </div>
-                <div className="App-box-pagina-principal media4" onClick="reproduzVideo()">
-                    <img src="https://picsum.photos/400/400?a=4 " alt="" />
-                    <h4>Título do vídeo</h4>
-                </div>
-                <div className="App-box-pagina-principal media5" onClick="reproduzVideo()">
-                    <img src="https://picsum.photos/400/400?a=5 " alt="" />
-                    <h4>Título do vídeo</h4>
-                </div>
-                <div className="App-box-pagina-principal media6" onClick="reproduzVideo()">
-                    <img src="https://picsum.photos/400/400?a=6 " alt="" />
-                    <h4>Título do vídeo</h4>
-                </div>
-                <div className="App-box-pagina-principal media7" onClick="reproduzVideo()">
-                    <img src="https://picsum.photos/400/400?a=7 " alt="" />
-                    <h4>Título do vídeo</h4>
-                </div>
-                <div className="App-box-pagina-principal media8" onClick="reproduzVideo()">
-                    <img src="https://picsum.photos/400/400?a=8 " alt="" />
-                    <h4>Título do vídeo</h4>
-                </div>
-            </section>
+            <div className="App-box-pagina-principal media1" onClick={reproduzVideo}>
+                <img src="https://picsum.photos/400/400?a=1 " alt="" />
+                <h4>Título do vídeo</h4>
+            </div>
+            <div className="App-box-pagina-principal media2" onClick={reproduzVideo}>
+              <img src="https://picsum.photos/400/400?a=2 " alt="" />
+              <h4>Título do vídeo</h4>
+          </div>
+          <div className="App-box-pagina-principal media3" onClick={reproduzVideo}>
+              <img src="https://picsum.photos/400/400?a=3 " alt="" />
+              <h4>Título do vídeo</h4>
+          </div>
+          <div className="App-box-pagina-principal media4" onClick={reproduzVideo}>
+              <img src="https://picsum.photos/400/400?a=4 " alt="" />
+              <h4>Título do vídeo</h4>
+          </div>
+          <div className="App-box-pagina-principal media5" onClick={reproduzVideo}>
+              <img src="https://picsum.photos/400/400?a=5 " alt="" />
+              <h4>Título do vídeo</h4>
+          </div>
+          <div className="App-box-pagina-principal media6" onClick={reproduzVideo}>
+              <img src="https://picsum.photos/400/400?a=6 " alt="" />
+              <h4>Título do vídeo</h4>
+          </div>
+          <div className="App-box-pagina-principal media7" onClick={reproduzVideo}>
+              <img src="https://picsum.photos/400/400?a=7 " alt="" />
+              <h4>Título do vídeo</h4>
+          </div>
+          <div className="App-box-pagina-principal media8" onClick={reproduzVideo}>
+              <img src="https://picsum.photos/400/400?a=8 " alt="" />
+              <h4>Título do vídeo</h4>
+          </div>
+        </section>
       </main>
+
+      <footer className="App-footer">
+        <h4>Oi! Eu moro no footer!</h4>
+      </footer>
     </div>
   );
 }
