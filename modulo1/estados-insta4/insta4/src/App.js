29d7b890607e08ee244004e34eee39178ab992d5
Camila Yung Alves <camilayung92@gmail.com>
2022-06-05 14:57:18 -0300

    Realização da atividade de quinta 02.06

--- modulo1/estados-insta4/insta4/src/App.js
+++ modulo1/estados-insta4/insta4/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components'
 import Post from './components/Post/Post';
 
@@ -8,28 +8,74 @@ const MainContainer = styled.div`
   flex-direction: column;
   align-items: center;
 `
+
 function App() {
-return(
-  <MainContainer>
-          <Post
-            nomeUsuario={'paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50?a=1'}
-            fotoPost={'https://picsum.photos/200/150?a=1'}
-          />
 
-          <Post
-            nomeUsuario={'laurinha'}
-            fotoUsuario={'https://picsum.photos/50/50?a=2'}
-            fotoPost={'https://picsum.photos/200/150?a=2'}
-          />
+  const [listaPost, setListaPost] = useState([
+    {
+      user: 'paulinha',
+      fotoUser: 'https://picsum.photos/50/50?a=1',
+      fotoPost: 'https://picsum.photos/200/150?a=1'
+    },
+    {
+      user: 'laurinha',
+      fotoUser: 'https://picsum.photos/50/50?a=2',
+      fotoPost: 'https://picsum.photos/200/150?a=2'
+     },
+     {
+      user: 'flavinha',
+      fotoUser: 'https://picsum.photos/50/50?a=3',
+      fotoPost: 'https://picsum.photos/200/150?a=3'
+     }
+  ])
 
-          <Post
-            nomeUsuario={'flavinha'}
-            fotoUsuario={'https://picsum.photos/50/50?a=3'}
-            fotoPost={'https://picsum.photos/200/150?a=3'}
-          />
-        </MainContainer>
-)
+  const [inputUser, setInputUser] = useState("")
+  const [inputFotoUser, setInputFotoUser] = useState("")
+  const [inputFotoPost, setInputFotoPost] = useState("")
+
+  const handleInputUser = (event) => {
+    setInputUser(event.target.value)
+  }
+
+  const handleInputFotoUser = (event) => {
+    setInputFotoUser(event.target.value)
+  }
+
+  const handleInputFotoPost = (event) => {
+    setInputFotoPost(event.target.value)
+  }
+
+  const insta = listaPost.map((post) => {
+    return <Post
+        nomeUsuario={post.user}
+        fotoUsuario={post.fotoUser}
+        fotoPost={post.fotoPost}
+      />
+    }
+  )
+
+  const inserirPost = () => {
+    const inputPost = {user: inputUser, fotoUser: inputFotoUser, fotoPost: inputFotoPost}
+    
+    // const novaListaPessoas = [...pessoas]
+    // novaListaPessoas.push(inputPessoa)
+    // setPessoas(novaListaPessoas)
+
+    // const novaListaPessoas = [...pessoas, inputPessoa]
+    // setPessoas(novaListaPessoas)
+
+    setListaPost([...listaPost, inputPost])
+  }
+
+  return(
+    <MainContainer>
+      <label>@user</label><input value={inputUser} onChange={handleInputUser}></input>
+      <label>foto de perfil</label><input value={inputFotoUser} onChange={handleInputFotoUser}></input>
+      <label>foto do post</label><input value={inputFotoPost} onChange={handleInputFotoPost}></input>
+      <button onClick={inserirPost}>carregar post</button>
+      {insta}
+    </MainContainer>
+  )
 
 }
 
