99ec0b94ce02c4ba4eabef03f52b824004e0b7cc
Camila Yung Alves <camilayung92@gmail.com>
2022-09-01 11:08:28 -0300

    Update files

---
+++ modulo3/knex-js/src/connection.ts
@@ -0,0 +1,18 @@
+import knex from "knex";
+//import dotenv from "dotenv";
+
+//dotenv.config();
+
+const connection = knex({
+   client: "mysql",
+   connection: {
+      host: '35.226.416.116',
+      port: 3306,
+      user: 'franklin-camila-alves',
+      password: 'Go86XXJF7~UgbwPA8tux',
+      database: 'franklin-camila-alves',
+      multipleStatements: true
+   },
+});
+
+export default connection
